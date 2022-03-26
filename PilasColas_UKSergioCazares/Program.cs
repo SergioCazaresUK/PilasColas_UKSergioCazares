1c2ae16c39fb96ab41d72115023909a0d4cc885b
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-03-26 13:35:34 -0700

    Corrimos el programa

--- PilasColas_UKSergioCazares/Program.cs
+++ PilasColas_UKSergioCazares/Program.cs
@@ -3,4 +3,8 @@
 Pilas pilas = new Pilas(10);
 pilas.Agregar("cero");
 pilas.Agregar("uno");
+pilas.Agregar("dos");
+pilas.Agregar("tres");
+pilas.Agregar("cuatro");
+pilas.Agregar("cinco");
 Console.WriteLine(pilas.imprimir());
